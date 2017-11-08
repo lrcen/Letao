7f019c240d3947847f8157c6e565b7dbec337934
xuanaheibai <lxuanp@163.com>
2017-11-08 13:06:32 +0800

    修改

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -5,7 +5,7 @@ $(function () {
 
     //表单校验功能
     //bootstrapValidator插件
-    $fomr.bootstrapValidator({
+    $form.bootstrapValidator({
         //显示的小图标
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
@@ -14,32 +14,56 @@ $(function () {
         },
 
         //设置字段名的校验规则
-        username: {
-            validators: {
-                //不能为空条件
-                notEmpty: {
-                    //提示信息, 为空时
-                    message: '用户名不能为空!'
+        fields: {
+            username: {
+                validators: {
+                    //不能为空条件
+                    notEmpty: {
+                        //提示信息, 为空时
+                        message: '用户名不能为空!'
+                    }
                 }
-            }
-        },
-
-        password: {
-            validators: {
-                notEmpty: {
-                    message: '密码不能为空!'
-                },
-
-                //校验密码的长度
-                stringLength: {
-                    //最小长度
-                    min: 6,
-                    //最大长度
-                    max: 16,
-                    message: '密码长度为6 - 16位'
+            },
+    
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: '密码不能为空!'
+                    },
+    
+                    //校验密码的长度
+                    stringLength: {
+                        //最小长度
+                        min: 6,
+                        //最大长度
+                        max: 16,
+                        message: '密码长度为6 - 16位'
+                    }
                 }
             }
         }
+    });
+
+    //重置按钮, 重置输入框以及样式
+    $("[type='reset']").on("click", function() {
+        // $form.data('bootstrapValidator'); 会得到一个validator实例
+        $form.data('bootstrapValidator').resetForm();
+    });
+
+    //阻止表单的默认行为, 要使用ajax
+    $('[type="submit"]').on("click", function(e) {
+        e.preventDefault();
+
+        //当点击登录时, 发送ajax请求
+        $.ajax({
+            url: "/employee/employeeLogin",
+            type: 'post',
+            // form对象.serialize()方法可以获取表单中所有带有name属性的表单元素, 并以 key=value&key2=value2&.. 进行拼接
+            data: $form.serialize(),
+            success: function(backData) {
+                console.log(backData);
+            }
+        });
     });
 
 });
