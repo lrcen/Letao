a8d46524ef4a2be34c9fb338cf3f4a5cf3e28697
xuanaheibai <lxuanp@163.com>
2017-11-11 14:27:16 +0800

    Letao

---
+++ public/back/js/productManage.js
@@ -0,0 +1,183 @@
+$(function() {
+    var pageNum = 1;
+    var pageSize = 5;
+
+    var render = function() {
+        $.ajax({
+            url: '/product/queryProductDetailList',
+            data: {
+                page: pageNum,
+                pageSize: pageSize
+            },
+            success: function(backData) {
+                $("tbody").html(template('prolistTmp', backData));
+
+                $('#paginator').bootstrapPaginator({
+                    bootstrapMajorVersion: 3,
+                    totalPages: Math.ceil(backData.total / pageSize),
+                    currentPage: pageNum,
+                    onPageClicked: function(arg1, arg2, arg3, page) {
+                        pageNum = page;
+
+                        render();
+                    }
+                })
+            }
+        });
+    }
+    render();
+
+    $('.btn-add').on('click', function() {
+        $('#addmodal').modal('show');
+
+        $.ajax({
+            url: '/category/querySecondCategoryPaging',
+            type: 'get',
+            data: {
+                page: 1,
+                pageSize: 100
+            },
+            success: function(backData) {
+                $(".dropdown-menu").html(template('secondSortTmp', backData));
+            }
+
+        });
+    })
+
+    $('.dropdown-menu').on('click', 'a', function() {
+        $('.choosesort').text($(this).text());
+
+        $('#brandId').val($(this).data('id'));
+
+        $form.data('bootstrapValidator').updateStatus('brandId', 'VALID');
+    })
+
+    //校验表单
+    var $form = $('form');
+    $form.bootstrapValidator({
+        excluded: [],
+
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+
+        fields: {
+            brandId: {
+                validators: {
+                    notEmpty: {
+                        message: '请选择分类 !'
+                    }
+                }
+            },
+
+            proName: {
+                validators: {
+                    notEmpty: {
+                        message: '商品名称不能为空 !'
+                    }
+                }
+            },
+
+            proDesc: {
+                validators: {
+                    notEmpty: {
+                        message: '商品描述不能为空 !'
+                    }
+                }
+            },
+
+            num: {
+                validators: {
+                    notEmpty: {
+                        message: '商品库存不能为空 !'
+                    },
+
+                    regexp: {
+                        regexp: /^[1-9]\d{0,3}$/,
+                        message: '格式不符, 请输入小于10000的正整数, 如: 9999'
+                    }
+                }
+            },
+
+            size: {
+                validators: {
+                    notEmpty: {
+                        message: '商品尺码不能为空 !'
+                    },
+
+                    regexp: {
+                        regexp: /^[1-7]\d-[1-7]\d$/,
+                        message: '格式不符, 请输入一个尺码区间, 区间范围为10-79 如: 10 - 50'
+                    }
+                }
+            },
+
+            oldPrice: {
+                validators: {
+                    notEmpty: {
+                        message: '商品原价不能为空 !'
+                    }
+                }
+            },
+
+            price: {
+                validators: {
+                    notEmpty: {
+                        message: '商品原价不能为空 !'
+                    }
+                }
+            },
+
+            forcheck: {
+                validators: {
+                    notEmpty: {
+                        message: '请选择三张图片 !'
+                    }
+                }
+            }
+        }
+    })
+
+
+    // 重置样式 内容等
+    function resetAll(){
+        $form.data('bootstrapValidator').resetForm();
+        $form[0].reset();
+
+        $('.choosesort').text('请选择二级分类');
+        $('#brandId').val('');
+
+        $('.img-box img').remove();
+        $('#forcheck').val('');
+    }
+
+    $('.btn-cancel').on('click', function() {
+        resetAll();
+    })
+
+    $('#btn-pic').fileupload({
+        dateType: 'json',
+        done: function(e, data) {
+            // console.log(data.result);
+            $('.img-box').prepend('<div class="preview" data-name="'+data.result.picName+'" data-addr="'+data.result.picAddr+'"> <i class="glyphicon glyphicon-remove"></i> <img src="'+data.result.picAddr+'"> </div>');
+
+            if($('.img-box .preview').length === 3) {
+                $form.data('bootstrapValidator').updateStatus('forcheck', 'VALID');
+            }else {
+                $form.data('bootstrapValidator').updateStatus('forcheck', 'INVALID');
+            }
+
+            $('.preview i').on('click', function() {
+                $(this).parent().empty().remove();
+
+                if($('.img-box .preview').length === 3) {
+                    $form.data('bootstrapValidator').updateStatus('forcheck', 'VALID');
+                }else {
+                    $form.data('bootstrapValidator').updateStatus('forcheck', 'INVALID');
+                }
+            })
+        }
+    })
+});
